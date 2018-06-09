31f96c5985ae1d4aa373f7262e22ade363cd6df7
Vitaliy Belyaev <vitalik1392@mail.ru>
2018-06-09 22:31:52 +0300

    Initial commit

---
+++ app/src/main/java/com/androidacademy/team5/zebratime/MainActivity.java
@@ -0,0 +1,13 @@
+package com.androidacademy.team5.zebratime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
