ba822965cb15c7b1918202c5fe0ebc4c0f6eb963
Vitaliy Belyaev <vitalik1392@mail.ru>
2018-06-10 18:24:11 +0300

    Add switch to NewProjectActivity

--- app/src/main/java/com/androidacademy/team5/zebratime/AllProjectsFragment.java
+++ app/src/main/java/com/androidacademy/team5/zebratime/AllProjectsFragment.java
@@ -1,28 +1,29 @@
 package com.androidacademy.team5.zebratime;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.androidacademy.team5.zebratime.ProjectsAdapter.ProjectOnClickHandler;
 import com.androidacademy.team5.zebratime.domain.Project;
 
 import java.util.ArrayList;
-import java.util.List;
-import com.androidacademy.team5.zebratime.ProjectsAdapter.ProjectOnClickHandler;
 
 public class AllProjectsFragment extends Fragment {
 
     private ProjectsAdapter adapter;
     private RecyclerView recyclerView;
     private ProjectOnClickHandler onClickHandler;
+    private Button createProjectButton;
 
     @Nullable
     @Override
@@ -31,6 +32,7 @@ public class AllProjectsFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_all_projects,container,false);
 
+        createProjectButton = view.findViewById(R.id.create_project);
         recyclerView = view.findViewById(R.id.projects_recycler_view);
         return view;
     }
@@ -50,6 +52,14 @@ public class AllProjectsFragment extends Fragment {
         projects.add(new Project("4","Project 4"));
 
         adapter.replaceWith(projects);
+
+        createProjectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext() ,NewProjectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/androidacademy/team5/zebratime/NewProjectActivity.java
+++ app/src/main/java/com/androidacademy/team5/zebratime/NewProjectActivity.java
@@ -1,8 +1,8 @@
 package com.androidacademy.team5.zebratime;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
