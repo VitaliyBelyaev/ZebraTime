b14a2625f185a4a12c2fea333260d120026f82d3
JuliaRayskaya <juliarayskaya@gmail.com>
2018-06-11 11:44:22 +0300

    Hints with animation

--- app/src/main/java/com/androidacademy/team5/zebratime/NewTaskActivity.java
+++ app/src/main/java/com/androidacademy/team5/zebratime/NewTaskActivity.java
@@ -3,6 +3,7 @@ package com.androidacademy.team5.zebratime;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -32,6 +33,14 @@ public class NewTaskActivity extends AppCompatActivity {
         btnOk = findViewById(R.id.btnOk);
         textTitle = findViewById(R.id.textTitle);
         textComment = findViewById(R.id.textComment);
+
+        TextInputLayout til1 = findViewById(R.id.text_input_textTitle);
+        EditText etl = findViewById(R.id.textTitle);
+        til1.setHint(getString(R.string.enter_title));
+
+        TextInputLayout til2 = findViewById(R.id.text_input_textComment);
+        EditText etl2 = findViewById(R.id.textComment);
+        til2.setHint(getString(R.string.enter_comment));
 
 
 
