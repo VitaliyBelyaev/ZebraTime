d2b9b29bb7aa4d229bc3a3a30700df3f6adcfca4
Vitaliy Belyaev <vitalik1392@mail.ru>
2018-06-11 05:19:52 +0300

    Merge remote-tracking branch 'remotes/origin/timer'
    # Conflicts:
    #	app/src/main/java/com/androidacademy/team5/zebratime/Timer.java
    #	app/src/main/java/com/androidacademy/team5/zebratime/entity/Task.java

--- app/src/main/java/com/androidacademy/team5/zebratime/TimerActivity.java
+++ app/src/main/java/com/androidacademy/team5/zebratime/TimerActivity.java
@@ -2,11 +2,13 @@ package com.androidacademy.team5.zebratime;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -22,6 +24,8 @@ import java.util.List;
 
 import static com.androidacademy.team5.zebratime.MainActivity.PROJECT_ID;
 import static com.androidacademy.team5.zebratime.MainActivity.TASK_ID;
+import static com.androidacademy.team5.zebratime.Timer.State.OVERWORK;
+import static com.androidacademy.team5.zebratime.Timer.State.STOP;
 
 public class TimerActivity extends AppCompatActivity {
 
@@ -30,54 +34,98 @@ public class TimerActivity extends AppCompatActivity {
     private TextView taskTitleTextView;
     private TextView taskDurationTextView;
     private Task task;
+    private Timer timer;
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private DatabaseReference tasksRef = database.getReference("tasks");
-    private DatabaseReference projectRefJ = database.getReference("Projects");
+    private DatabaseReference tasksRef = database.getReference("Tasks");
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
+        timer = getApp().timer;
         timeTextView = findViewById(R.id.tv_time);
         actionButton = findViewById(R.id.action_button);
         taskTitleTextView = findViewById(R.id.tv_timer_task_title);
         taskDurationTextView = findViewById(R.id.tv_timer_task_duration);
 
-        String projectId = getIntent().getStringExtra(PROJECT_ID);
-        final String taskId = getIntent().getStringExtra(TASK_ID);
+        if (timer.getTask() == null) {
+            String projectId = getIntent().getStringExtra(PROJECT_ID);
+            final String taskId = getIntent().getStringExtra(TASK_ID);
 
-        DatabaseReference taskRef = projectRefJ.child(projectId).child("tasks").child(taskId);
+            DatabaseReference taskRef = tasksRef.child(taskId);
 
+            ValueEventListener taskListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
 
-        ValueEventListener taskListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                task = dataSnapshot.getValue(Task.class);
-                Log.i("TASK_TEST", "Task: " + task);
-                taskTitleTextView.setText(task.getTitle());
-                List<Session> sessions = task.getSessions();
-                long duration = 0;
-                if (sessions != null) {
-                    for (Session session : sessions) {
-                        duration = duration + session.getDuration();
-                    }
+                    task = dataSnapshot.getValue(Task.class);
+                    Log.i("TASK_TEST", "Task: " + task);
+                    showTaskInfo(task);
+                    timer.setTask(task);
                 }
 
-                taskDurationTextView.setText(String.valueOf(duration));
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.w("Error", "loadPost:onCancelled", databaseError.toException());
+                }
+            };
+            taskRef.addListenerForSingleValueEvent(taskListener);
 
+        } else {
+            task = getApp().timer.getTask();
+            showTaskInfo(task);
+        }
 
+        final CountDownTimer internalTimer = new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                Log.i("TimerTest", "seconds remaining: " + millisUntilFinished / 1000);
+                timeTextView.setText(String.valueOf(millisUntilFinished / 1000));
             }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.w("Error", "loadPost:onCancelled", databaseError.toException());
+            public void onFinish() {
+                timer.setState(OVERWORK);
+                actionButton.setText("Take break");
+                Log.i("TimerTest", "Done!");
             }
         };
-        taskRef.addListenerForSingleValueEvent(taskListener);
 
+        final CountDownTimer breakTimer = new CountDownTimer(7000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                Log.i("TimerTest", "seconds remaining: " + millisUntilFinished / 1000);
+                timeTextView.setText(String.valueOf(millisUntilFinished / 1000));
+            }
+
+            public void onFinish() {
+                timer.setState(STOP);
+                Log.i("TimerTest", "Done!");
+            }
+        };
+
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(timer.getState()){
+                    case STOP:
+                        actionButton.setText("STOP");
+                        timer.start(internalTimer);
+                        break;
+                    case WORK:
+                        actionButton.setText("START");
+                        timer.stop();
+                        break;
+                    case OVERWORK:
+                        actionButton.setText("START");
+                        timer.pause(breakTimer);
+                    case PAUSE:
+                        actionButton.setText("STOP");
+                        timer.start(internalTimer);
+                }
+            }
+        });
     }
 
     @Override
@@ -95,5 +143,21 @@ public class TimerActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showTaskInfo(Task task) {
+        taskTitleTextView.setText(task.getTitle());
+        List<Session> sessions = task.getSessions();
+        long duration = 0;
+        if (sessions != null) {
+            for (Session session : sessions) {
+                duration = duration + session.getDuration();
+            }
+        }
+        taskDurationTextView.setText(String.valueOf(duration));
+    }
+
+    private App getApp() {
+        return (App) getApplication();
     }
 }
